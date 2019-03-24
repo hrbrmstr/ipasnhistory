81e0b9dff489f70dc3f8eabf3a26adb690e710ac
boB Rudis <bob@rud.is>
2019-03-24 06:28:39 -0400

    R package repo initialization complete

---
+++ R/ipasnhistory-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/ipasnhistory>
+#' - BugReports: <https://gitlab.com/hrbrmstr/ipasnhistory/issues>
+#' 
+#' @md
+#' @name ipasnhistory
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
